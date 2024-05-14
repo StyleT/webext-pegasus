378d3bd75f889371a30f0559351d2040f27bff53
Vlad Fedosov <vladlen_fedosov@epam.com>
2024-05-13 21:12:06 -0400

    chore: Removed copyrights copied by accident from Lexical repo while copying boilerplate code

--- packages/example-extension/src/entrypoints/devtools-panel/main.tsx
+++ packages/example-extension/src/entrypoints/devtools-panel/main.tsx
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import {initPegasusTransport} from '@webext-pegasus/transport/devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
